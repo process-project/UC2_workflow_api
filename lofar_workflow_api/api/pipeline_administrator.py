e56bce1e6966d982756d5063ef5fee07036e1d5c
souley <s.madougou@esciencecenter.nl>
2020-01-06 01:45:09 -0500

    New package name

--- lofar_workflow_api/api/pipeline_administrator.py
+++ lofar_workflow_api/api/pipeline_administrator.py
@@ -4,9 +4,9 @@
 #    return {LRT.give_name(): LRT}
 
 import LGPPP_LOFAR_pipeline
-import PREFACTOR_LOFAR_pipeline
+import UC2_pipeline
  
 def get_available_pipelines():
     return {LGPPP_LOFAR_pipeline.give_name(): LGPPP_LOFAR_pipeline,
-		PREFACTOR_LOFAR_pipeline.give_name(): PREFACTOR_LOFAR_pipeline}
+		UC2_pipeline.give_name(): UC2_pipeline}
 
